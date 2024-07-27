09aa74059074cea5a5ab62f3088d850234ad7e91
AI@AscendingSoftware <pranith@ascendingsoftware.com>
2024-07-27 15:16:36 +0530

    statistics values are tested

--- main_.py
+++ main_.py
@@ -5,9 +5,7 @@ from getting_gestures_score import GestureAnalyzer
 from video_to_audio_analyse import SpeechAnalyzer
 from convert_video_to_base64 import video_to_base64, base64_to_video, converting_image_base64_into_image
 from api import get_details,post_final_data, post_one_video_result
-import os,json,time
-
-
+import time
 
 
 def find_average(combined_dict):
@@ -31,6 +29,8 @@ def find_average(combined_dict):
     }
     
     return average_dict
+
+
 
 def main(video_path, videoI_userId):
     print('you are in main with:',video_path, videoI_userId)
@@ -62,7 +62,7 @@ def main(video_path, videoI_userId):
     combined_dict = {**gesture_results, **speech_scores, **vedio_details}
 
     fcialScore=(combined_dict['happy']+combined_dict['nautral']+combined_dict['surprise']+combined_dict['angry']+combined_dict['fear']+combined_dict['disgust']+combined_dict['sad']+combined_dict['faceConfidence'])/8
-    communicationScore=(combined_dict['fluency']*100 + combined_dict['grammar']*100 +combined_dict['pronunciation'])/3
+    communicationScore=(combined_dict['fluency']*100 + combined_dict['grammar']*100 +combined_dict['pronunciation']*100)/3
     speechScore=(combined_dict['tone']*100 + combined_dict['voiceConfidence']*100)/2
     bodyLanguageScore=(combined_dict['lookingStraight']+combined_dict['smileCount']+combined_dict['handUsage']+combined_dict['armsCrossed']+combined_dict['wristsClosed']+combined_dict['weightOnOneLeg']+combined_dict['legMovement']+combined_dict['weightBalancedOnBothLegs']+combined_dict['eyeContact'])/9
     overAllScroe=(fcialScore+communicationScore+speechScore+bodyLanguageScore)/4
@@ -90,12 +90,12 @@ def main(video_path, videoI_userId):
     "sad": round(combined_dict['sad'],2),
     "faceConfidence": round(combined_dict["faceConfidence"],2),
     "communicationScore": round(wanted_data["communicationScore"],2),
-    "grammar": round(combined_dict["grammar"],2),
-    "fluency": round(combined_dict["fluency"],2),
-    "pronunciation": round(combined_dict["pronunciation"],2),
+    "grammar": round(combined_dict["grammar"]*100,2),
+    "fluency": round(combined_dict["fluency"]*100,2),
+    "pronunciation": round(combined_dict["pronunciation"]*100,2),
     "speechScore": round(wanted_data["speechScore"],2),
-    "tone": round(combined_dict["tone"],2),
-    "voiceConfidence": round(combined_dict["voiceConfidence"],2),
+    "tone": round(combined_dict["tone"]*100,2),
+    "voiceConfidence": round(combined_dict["voiceConfidence"]*100,2),
     "speechRate": round(combined_dict["speechRate"],2),
     "bodyLanguageScore": round(wanted_data["bodyLanguageScore"],2),
     "lookingStraight": round(combined_dict["lookingStraight"],2),
@@ -111,20 +111,20 @@ def main(video_path, videoI_userId):
     }
     
     print("one_video_data:",one_video_data, type(one_video_data), len(one_video_data))
-    # post_video_result=post_one_video_result(one_video_data)
-    # print('post_video_result:',post_video_result)
+    post_video_result=post_one_video_result(one_video_data)
+    print('post_video_result:',post_video_result)
 
 
-    # # print(vedio_details['userId'], type(vedio_details['userId']))
-    # result = get_details(vedio_details['userId'])
-    # print(result, type(result), len(result), 'final result data')
+    # print(vedio_details['userId'], type(vedio_details['userId']))
+    result = get_details(vedio_details['userId'])
+    print(result, type(result), len(result), 'final result data')
 
-    # # final_out=json.dumps(result(combined_dict), indent=4)
-    # # print(final_out, len(final_out), type(final_out), 'final ou data')
-    # final_out = find_average(result)
-    # print('final_out:',final_out)
-    # #to post final score
-    # post_final_data(final_out)
+    # final_out=json.dumps(result(combined_dict), indent=4)
+    # print(final_out, len(final_out), type(final_out), 'final ou data')
+    final_out = find_average(result)
+    print('final_out:',final_out)
+    #to post final score
+    post_final_data(final_out)
 
     end_time = time.time()
     # # Calculate the time taken in seconds
